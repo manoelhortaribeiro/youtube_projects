70d7cf55ba0411606338427e0b602976f5e97094
Lam Olivier Quôc-Vinh <olam@iccluster037.iccluster.epfl.ch>
2020-11-18 17:27:32 +0100

    classifier done, waiting results in the final pipeline

--- scripts/yt_lda_tune.py
+++ scripts/yt_lda_tune.py
@@ -10,22 +10,26 @@ from pyspark.ml.clustering import LDA, LDAModel, LocalLDAModel
 from pyspark.ml.linalg import Vectors, SparseVector
 
 
-
 def main():
     conf = SparkConf()
 
     # create the session
-    spark = SparkSession.builder.appName("LDA_topicmodelling").config(conf=conf).getOrCreate()
+    spark = SparkSession.builder.appName(
+        "LDA_topicmodelling").config(conf=conf).getOrCreate()
 
     # Load data
-    n_topics_list = [155, 160, 165, 170, 175]
-    max_iter = 1000
-    vocabSize = 42757
+    n_topics_list = [50, 55, 60, 65, 70, 75, 80, 85, 90, 95,
+                     100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150]
 
-    print('Initializing... Tuning the number of topics with ' + str(max_iter)  + ' iters')
+    max_iter = 500
+    vocabSize = 53255
+
+    print('Initializing... Tuning the number of topics with ' +
+          str(max_iter) + ' iters')
 
     print('Loading data...')
-    df_load_view10000_sub100000 = spark.read.json('/user/olam/view10000_sub100000/sparkdf.json')
+    df_load_view10000_sub100000 = spark.read.json(
+        '/user/olam/final_res/sparkdf.json')
 
     # Process data
     data_view10000_sub100000 = []
@@ -33,29 +37,37 @@ def main():
     print('Processing data...')
     for row in df_load_view10000_sub100000.collect():
         features = row['features']
-        data_view10000_sub100000.append([row['id'], SparseVector(features['size'], features['indices'], features['values'])])
+        data_view10000_sub100000.append([row['id'], SparseVector(
+            features['size'], features['indices'], features['values'])])
 
-    df_view10000_sub100000 = spark.createDataFrame(data_view10000_sub100000, ['id', 'features'])
+    df_view10000_sub100000 = spark.createDataFrame(
+        data_view10000_sub100000, ['id', 'features'])
 
     for n_topics in n_topics_list:
 
         print('Training with ' + str(n_topics) + ' topics...')
-        lda_view10000_sub100000 = LDA(k=n_topics, maxIter=max_iter ,seed=1)
-        model_view10000_sub100000 = lda_view10000_sub100000.fit(df_view10000_sub100000)
+        lda_view10000_sub100000 = LDA(k=n_topics, maxIter=max_iter, seed=1)
+        model_view10000_sub100000 = lda_view10000_sub100000.fit(
+            df_view10000_sub100000)
 
         # save the model attributes
         print('Save the model attributes...')
 
         model_view10000_sub100000.describeTopics(maxTermsPerTopic=vocabSize).write\
-                        .option('compression', 'gzip')\
-                        .json('/user/olam/view10000_sub100000/tune/describe_topics_' + str(n_topics) + '_iter1000_tok100vid.json')
+            .option('compression', 'gzip')\
+            .json('/user/olam/final_res/describe_topics_' + str(n_topics) + '.json')
 
         model_view10000_sub100000.transform(df_view10000_sub100000).write\
-                        .option('compression', 'gzip')\
-                        .json('/user/olam/view10000_sub100000/tune/topics_doc_matrix_' + str(n_topics) + '_iter1000_tok100vid.json')
+            .option('compression', 'gzip')\
+            .json('/user/olam/final_res/topics_doc_matrix_' + str(n_topics) + 'json')
 
+        print('Save the model...')
+
+        model_view10000_sub100000.save(
+            '/user/olam/final_res/model_' + str(n_topics))
 
     print('Task done!')
+
 
 if __name__ == "__main__":
     main()
